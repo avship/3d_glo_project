904e85e3bbd3d9e7b851f2faa3b389ea5598ebfd
avship <alexandershipilo@yandex.ru>
2022-07-21 15:45:39 +0300

    Добавлена валидация форм

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timerModule from "./modules/timer";
 import menuModule from "./modules/menu";
 import modalModule from "./modules/modal";
+import formValidation from "./modules/form-validation";
 
 timerModule("31 july 2022");
 menuModule();
 modalModule();
+formValidation();
