a6c247fae955c81acb04c34e315d85d3ad38485e
avship <alexandershipilo@yandex.ru>
2022-07-21 12:49:57 +0300

    Добавлены модальные окна с анимацией

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import timerModule from "./modules/timer";
 import menuModule from "./modules/menu";
+import modalModule from "./modules/modal";
 
 timerModule("31 july 2022");
 menuModule();
+modalModule();
